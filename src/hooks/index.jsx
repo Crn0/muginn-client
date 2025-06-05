3724cca3d3fda3c9ce14b4dcdbd4a4fc8f1f90ef
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-05 17:58:33 +0800

    feat(hooks): add useSilentLogin hook to redirect authenticated users

---
+++ src/hooks/index.jsx
@@ -0,0 +1,1 @@
+export { default as useSilentLogin } from "./user-silent-login";
---
+++ src/stores/auth.js
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+const initialState = {
+  token: null,
+  isRefreshingToken: null,
+};
+
+export const useAuthStore = create(
+  devtools((set) => ({
+    ...initialState,
+    setToken: (token) => set(() => ({ token })),
+    setIsRefreshingToken: (isRefreshingToken) => set(() => ({ isRefreshingToken })),
+    reset: () => {
+      set(initialState);
+    },
+  }))
+);
+
+export const getToken = () => useAuthStore.getState().token;
+export const getIsRefreshingToken = () => useAuthStore.getState().token;
+
+export const setToken = (token) => useAuthStore.setState({ token });
+export const setIsRefreshingToken = (isRefreshingToken) =>
+  useAuthStore.setState({ isRefreshingToken });
