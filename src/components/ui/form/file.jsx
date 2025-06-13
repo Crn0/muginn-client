7de88306129b1e5990794724736889c28addbd8f
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-13 16:17:04 +0800

    style(components/ui/form/file, file-wrapper): remove hidden display on input; set width and height to 0

--- src/components/ui/form/file.jsx
+++ src/components/ui/form/file.jsx
@@ -1,28 +1,36 @@
 import PropTypes from "prop-types";
 import { forwardRef } from "react";
-
+import { useFormContext } from "react-hook-form";
 import FileWrapper from "./file-wrapper";
+import errorHandler from "./error-handler";
 
 const File = forwardRef(
-  (
-    { label, renderFieldButton, onKeyDown, error, className, name, registration, accept, ...props },
-    ref
-  ) => {
+  ({ label, renderFieldButton, serverError, className, name, accept, onKeyDown }, ref) => {
+    const {
+      register,
+      formState: { errors },
+    } = useFormContext();
     const fieldButton = renderFieldButton();
+
+    const { ref: inputRef, ...rest } = register(name);
+
+    const error = errorHandler(name, {
+      serverError,
+      formError: errors,
+    });
 
     return (
       <div>
         {fieldButton}
         <FileWrapper label={label} className={className} error={error} onKeyDown={onKeyDown}>
           <input
+            {...rest}
             type='file'
             name={name}
-            className='hidden'
+            className='h-0 w-0 cursor-none opacity-0'
             aria-invalid={error?.[name] ? "true" : "false"}
             accept={accept}
-            ref={ref}
-            {...registration}
-            {...props}
+            ref={ref(inputRef)}
           />
         </FileWrapper>
       </div>
@@ -35,19 +43,14 @@ File.displayName = "File";
 File.propTypes = {
   label: PropTypes.string.isRequired,
   renderFieldButton: PropTypes.func.isRequired,
-  onKeyDown: PropTypes.func.isRequired,
-  error: PropTypes.objectOf(
-    PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.arrayOf(PropTypes.string),
-      PropTypes.instanceOf(Element),
-    ])
-  ),
   registration: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.func])),
   className: PropTypes.string,
   name: PropTypes.string.isRequired,
   accept: PropTypes.string.isRequired,
   required: PropTypes.bool,
+  serverError: PropTypes.instanceOf(Error),
+  onBlur: PropTypes.func,
+  onKeyDown: PropTypes.func,
 };
 
 export default File;
