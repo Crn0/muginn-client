57b4ada0fd10cfad8a81e18889e393805f9704c8
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-13 16:20:38 +0800

    refactor(components/ui/form/textarea): use useFormContext to handle internal form logic

--- src/components/ui/form/textarea.jsx
+++ src/components/ui/form/textarea.jsx
@@ -1,40 +1,47 @@
 import PropTypes from "prop-types";
 import { forwardRef } from "react";
+import { useFormContext } from "react-hook-form";
 
 import FieldWrapper from "./field-wrapper";
+import errorHandler from "./error-handler";
 
 const TextArea = forwardRef(
-  ({ label, error, className, name, maxLength, registration, required, ...props }, ref) => (
-    <FieldWrapper label={label} error={error} isRequired={required}>
-      <textarea
-        name={name}
-        className={className}
-        maxLength={maxLength}
-        aria-invalid={error?.[name] ? "true" : "false"}
-        ref={ref}
-        {...registration}
-        {...props}
-      />
-    </FieldWrapper>
-  )
+  ({ label, serverError, className, name, maxLength, required, ...props }, ref) => {
+    const {
+      register,
+      formState: { errors },
+    } = useFormContext();
+
+    const error = errorHandler(name, {
+      serverError,
+      formError: errors,
+    });
+
+    return (
+      <FieldWrapper label={label} error={error} isRequired={required}>
+        <textarea
+          name={name}
+          className={className}
+          maxLength={maxLength}
+          aria-invalid={error?.message ? "true" : "false"}
+          ref={ref}
+          {...register(name)}
+          {...props}
+        />
+      </FieldWrapper>
+    );
+  }
 );
 
 TextArea.displayName = "TextArea";
 
 TextArea.propTypes = {
   label: PropTypes.string.isRequired,
-  registration: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.func])),
   className: PropTypes.string,
   name: PropTypes.string.isRequired,
   maxLength: PropTypes.number.isRequired,
   required: PropTypes.bool,
-  error: PropTypes.objectOf(
-    PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.arrayOf(PropTypes.string),
-      PropTypes.instanceOf(Element),
-    ])
-  ),
+  serverError: PropTypes.instanceOf(Error),
 };
 
 export default TextArea;
