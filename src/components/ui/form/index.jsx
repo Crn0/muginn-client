789a14e1031e88a7f50712277d00d240b9b41109
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-13 16:25:01 +0800

    chore(components/ui/form/index): export form-related components

--- src/components/ui/form/index.jsx
+++ src/components/ui/form/index.jsx
@@ -1,3 +1,6 @@
 export { default as Form } from "./form";
 export { default as FieldWrapper } from "./field-wrapper";
 export { default as Input } from "./input";
+export { default as File } from "./file";
+export { default as TextArea } from "./textarea";
+export { default as FormConfirmation } from "./form-confirmation";
