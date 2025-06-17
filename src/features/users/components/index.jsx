6d50fbb13a7dc248fe667ec81508bdf6b1b66576
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-17 16:43:30 +0800

    fix(features/users/components/index): correct UpdateAccountProfile component import path

---
+++ src/features/users/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as UpdateMainProfile } from "./update-main-profile";
+export { default as UpdateAccountProfile } from "./update-account-profile";
+export { default as UserStanding } from "./user-standing";
