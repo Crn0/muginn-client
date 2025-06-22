83a7398c21aca0bb32d8ab3fa5cbe4f6fe5b0d60
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-22 16:07:02 +0800

    feat(components/errors): create reusable error components for boundary and async handling

---
+++ src/components/errors/error-element.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+import { useErrorBoundary } from "react-error-boundary";
+import { Button } from "../ui/button";
+
+export default function ErrorElement({ error }) {
+  const { resetBoundary } = useErrorBoundary();
+
+  return (
+    <div role='alert'>
+      <p>Something went wrong:</p>
+      <pre className='text-red-600'>{error.message}</pre>
+      <Button type='button' testId='retry' onClick={() => resetBoundary}>
+        Try again
+      </Button>
+    </div>
+  );
+}
+
+ErrorElement.propTypes = {
+  error: PropTypes.instanceOf(Error).isRequired,
+};
---
+++ src/components/errors/index.jsx
@@ -0,0 +1,3 @@
+export { default as AwaitErrorElement } from "./await-error";
+export { default as ErrorElement } from "./error-element";
+export { default as RouteErrorElement } from "./route-error";
