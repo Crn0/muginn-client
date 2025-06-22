bee8f998eaa48910311b99839451b48bc10e0490
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-22 16:09:39 +0800

    feat(pages/silent-login): create page to handle silent login with valid refresh token

---
+++ src/pages/__tests__/silent-login.test.jsx
@@ -0,0 +1,116 @@
+import { createMemoryRouter, replace } from "react-router-dom";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { screen, waitFor } from "@testing-library/react";
+import { QueryClient } from "@tanstack/react-query";
+
+import setupRouter from "./mocks/utils/setup-router";
+import { paths } from "../../configs/index";
+import { generateAccessToken } from "../../../test/utils";
+import { setToken } from "../../stores";
+import { clientLoader } from "../../features/auth/api";
+import SilentLogin from "../silent-login";
+import DashBoard, { dashBoardLoader } from "./mocks/dash-board";
+import { RouteErrorElement } from "../../components/errors";
+
+vi.mock("../../features/auth/api");
+
+const waitResolve = (data, ms = 1000) =>
+  new Promise((r) => {
+    setTimeout(() => r(typeof data === "function" ? data() : data), ms);
+  });
+
+const waitReject = (data, ms = 1000) =>
+  new Promise((_, r) => {
+    setTimeout(() => r(typeof data === "function" ? data() : data), ms);
+  });
+
+const queryClient = new QueryClient();
+
+const createRouter = (initialEntries) =>
+  createMemoryRouter(
+    [
+      {
+        path: paths.dashboard.path,
+        loader: dashBoardLoader(queryClient),
+        errorElement: <RouteErrorElement />,
+        element: <DashBoard />,
+      },
+      {
+        path: paths.silentLogin.path,
+        loader: clientLoader(queryClient),
+        errorElement: <RouteErrorElement />,
+        element: <SilentLogin />,
+      },
+      {
+        path: paths.login.path,
+        element: <p> Login Page</p>,
+      },
+    ],
+    {
+      initialEntries,
+    }
+  );
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+describe("SilentLogin page", () => {
+  describe("Error Boundary Trigger", () => {
+    it("throws when the user query fails with a network error (e.g., 'Failed to fetch')", async () => {
+      vi.mocked(clientLoader).mockImplementationOnce(() => async () => ({
+        data: waitReject(new Error("Failed to Fetch"), 10),
+      }));
+
+      setupRouter(createRouter([paths.silentLogin.getHref()]), queryClient);
+
+      await waitFor(() => {
+        expect(screen.getByTestId("spinner")).toBeInTheDocument();
+      });
+
+      await waitFor(() => {
+        expect(screen.getByText("Failed to Fetch")).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("Authentication Error", () => {
+    it("shows a loading indicator and redirects the user to /login", async () => {
+      vi.mocked(clientLoader).mockImplementationOnce(() => async () => ({
+        data: waitReject(() => replace(paths.login.getHref()), 10),
+      }));
+
+      setupRouter(createRouter([paths.silentLogin.getHref()]), queryClient);
+
+      await waitFor(() => {
+        expect(screen.getByTestId("spinner")).toBeInTheDocument();
+      });
+
+      await waitFor(() => {
+        expect(screen.getByText("Login Page")).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("Success Case", () => {
+    it("shows a loading indicator and redirects the user to dashboard", async () => {
+      const token = generateAccessToken(".crno.");
+
+      setToken(token);
+
+      vi.mocked(clientLoader).mockImplementationOnce(() => async () => ({
+        data: waitResolve(token, 10),
+      }));
+
+      setupRouter(createRouter([paths.silentLogin.getHref()]), queryClient);
+
+      await waitFor(() => {
+        expect(screen.getByTestId("spinner")).toBeInTheDocument();
+      });
+
+      await waitFor(() => {
+        expect(screen.getByText(".crno.")).toBeInTheDocument();
+      });
+    });
+  });
+});
---
+++ src/pages/silent-login.jsx
@@ -0,0 +1,57 @@
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense, useEffect } from "react";
+import {
+  useSearchParams,
+  useLoaderData,
+  Await,
+  useAsyncValue,
+  useNavigate,
+} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+import { paths } from "../configs";
+import { getAuthUserQueryOptions } from "../lib";
+import { AwaitErrorElement, ErrorElement } from "../components/errors";
+import { Spinner } from "../components/ui/spinner";
+
+function Wrapper() {
+  const token = useAsyncValue();
+
+  const user = useQuery({
+    ...getAuthUserQueryOptions(),
+    enabled: !!token,
+    throwOnError: (e) => e.response?.status >= 500 || e?.message === "Failed to Fetch",
+  });
+
+  const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+
+  const redirectTo = searchParams.get("redirectTo");
+
+  useEffect(() => {
+    if (user.isError) {
+      navigate(paths.login.getHref(redirectTo), { replace: true });
+    }
+
+    if (user.data) {
+      navigate(redirectTo || paths.dashboard.getHref(), { replace: true });
+    }
+  }, [navigate, redirectTo, user.data, user.isError]);
+
+  return <Spinner />;
+}
+
+export default function SilentLogin() {
+  const { data: loaderData } = useLoaderData();
+
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Await resolve={loaderData} errorElement={<AwaitErrorElement />}>
+        <ErrorBoundary FallbackComponent={ErrorElement}>
+          <Wrapper />
+        </ErrorBoundary>
+      </Await>
+    </Suspense>
+  );
+}
