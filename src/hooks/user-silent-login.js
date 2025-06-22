c5a358af28650d67b783f9630e0a8c00f27a3fe3
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-22 16:14:51 +0800

    refactor(hooks/use-silent-login): handle case when user has a valid refresh token

--- src/hooks/user-silent-login.js
+++ src/hooks/user-silent-login.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { useAuthStore } from "../stores";
-import { useGetUser } from "../lib/auth";
+import { useGetUser, useRefreshToken } from "../lib/auth";
 import { paths } from "../configs";
 
 export default function useSilentLogin() {
@@ -17,11 +17,21 @@ export default function useSilentLogin() {
 
   const isAuth = useAuthStore((state) => state.token) !== null;
 
+  const { mutate, isError, isPending } = useRefreshToken({
+    onSuccess: (token) => {
+      setToken(token);
+    },
+  });
+
   const user = useGetUser({
     enabled: isAuth,
   });
 
   useEffect(() => {
+    if (!isAuth && (!isPending || !isError)) {
+      mutate();
+    }
+
     if (user.data) {
       navigate(redirectTo ?? paths.dashboard.getHref(), {
         replace: true,
@@ -46,5 +56,8 @@ export default function useSilentLogin() {
     user.data,
     user.isSuccess,
     user.isError,
+    mutate,
+    isPending,
+    isError,
   ]);
 }
