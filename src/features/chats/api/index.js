ad526cffcb8d27e28739c1f081a6ea9cde4ebc0b
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-26 15:15:31 +0800

    feat(chats): add DirectChatList and GroupChatList components, their test cases, and getChats API

--- src/features/chats/api/index.js
+++ src/features/chats/api/index.js
@@ -1,2 +1,3 @@
 export { default as clientAction } from "./action";
 export * from "./create-chat";
+export * from "./get-chats";
---
+++ src/features/chats/components/group-chat-list.jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getChatsQueryOptions } from "../api/get-chats";
+import { Spinner } from "../../../components/ui/spinner";
+import { Link } from "../../../components/ui/link";
+import { Avatar } from "../../../components/ui/image";
+
+export default function GroupChatList() {
+  const chatsQuery = useQuery({
+    ...getChatsQueryOptions(),
+    // throwOnError: (e) => e.response?.status >= 500 || e.message.toLowerCase() === "failed to fetch",
+  });
+
+  if (chatsQuery.isLoading && !chatsQuery.data) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
+
+  const chats = chatsQuery.data.filter((chat) => chat.type === "GroupChat");
+
+  if (chats.length === 0) {
+    return null;
+  }
+
+  return (
+    <div>
+      {chats.map((chat) => (
+        <Link key={chat.id} to={`/chats/${chat.id}`}>
+          <Avatar type='group' asset={chat.avatar} alt={chat.name} />
+        </Link>
+      ))}
+    </div>
+  );
+}
