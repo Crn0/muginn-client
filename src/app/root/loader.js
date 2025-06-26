87e33531d80fb21c50a580ea338c48b7bd948b47
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-26 14:45:56 +0800

    refactor(app/root): check token existence and seed auth user cache in loader; show loading indicator in component

--- src/app/root/loader.js
+++ src/app/root/loader.js
@@ -2,16 +2,17 @@ import { replace } from "react-router-dom";
 
 import { paths } from "../../configs";
 import { getAuthUserQueryOptions } from "../../lib";
+import { getToken } from "../../stores";
 
 export default function loader(queryClient) {
   return ({ request }) => {
-    const user = queryClient.getQueryData(getAuthUserQueryOptions().queryKey);
-
     const redirectTo = new URL(request.url).pathname;
 
-    if (!user) {
+    if (!getToken()) {
       return replace(paths.silentLogin.getHref(redirectTo));
     }
+
+    queryClient.ensureQueryData(getAuthUserQueryOptions().queryKey);
 
     return null;
   };
