3287a42cef113331b806dab7aa05671be03383bf
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-26 15:52:39 +0800

    chore(test): add DB setup for chat, role, and permission; create handler for chat and seed function for permissions

---
+++ test/mocks/handlers/chat.js
@@ -0,0 +1,135 @@
+import z from "zod";
+import { http, HttpResponse } from "msw";
+
+import db from "../db";
+import { env } from "../../../src/configs";
+import { withAuth, withUser } from "./middleware";
+import { networkDelay } from "../utils";
+
+const chatType = z.enum(["DirectChat", "GroupChat"]);
+
+const baseUrl = `${env.getValue("serverUrl")}/api/v${env.getValue("apiVersion")}/users/me`;
+
+const directChatCondition = (data, ctx) => {
+  const dataSchema = z.object({
+    memberIds: z.array(z.string().uuid()).min(2).max(2),
+  });
+
+  const issues = dataSchema.safeParse(data)?.error?.issues;
+
+  if (issues?.length) {
+    issues.forEach((issue) => ctx.addIssue({ ...issue }));
+  }
+};
+
+const groupChatCondition = (data, ctx) => {
+  const nameIsOverHundredCharacters =
+    z
+      .string()
+      .max(100)
+      .safeParse(data.name ?? "").success === false;
+
+  if (nameIsOverHundredCharacters) {
+    ctx.addIssue({
+      code: "too_big",
+      maximum: 100,
+      type: "string",
+      inclusive: true,
+      exact: false,
+      message: "Name must contain at most 100 character(s)",
+      path: ["name"],
+    });
+  }
+};
+
+const requestBodySchema = z
+  .object({
+    type: chatType,
+    name: z.string().max(100, { message: "Name must contain at most 100 character(s)" }),
+    membersId: z.array(z.string().uuid()),
+  })
+  .superRefine((data, ctx) => {
+    if (data.type === "DirectChat") {
+      return directChatCondition(data, ctx);
+    }
+
+    return groupChatCondition(data, ctx);
+  });
+
+export default [
+  http.post(
+    baseUrl,
+    withAuth(
+      withUser(async ({ request, user }) => {
+        await networkDelay();
+        let body;
+        let createdChat;
+        try {
+          if (request.headers.get("Content-type").includes("multipart/form-data")) {
+            body = requestBodySchema.safeParse(await request.clone().formData());
+          } else {
+            body = requestBodySchema.safeParse(await request.clone().json());
+          }
+
+          if (!body.success) {
+            return HttpResponse.json(
+              {
+                message: `Validation failed: ${body.error.issues.length} errors detected in body`,
+
+                errors: body.error.issues,
+              },
+              { status: 422 }
+            );
+          }
+
+          const permissions = db.permissions.findMany({
+            name: {
+              in: ["send_message", "view_chat"],
+            },
+          });
+
+          const roles = [
+            db.role.create({
+              permissions,
+              chat: createdChat,
+            }),
+          ];
+
+          if (body.type === "GroupChat") {
+            createdChat = db.chat.create({
+              owner: user,
+              name: body.name,
+              type: body.type,
+              isPrivate: false,
+            });
+
+            db.userOnChat.create({
+              user,
+              roles,
+              chat: createdChat,
+            });
+          } else {
+            createdChat = db.chat.create({
+              type: body.type,
+              isPrivate: true,
+            });
+
+            const users = db.user.findMany({ where: { id: { in: [body.memberIds] } } });
+
+            users.forEach((u) =>
+              db.userOnChat.create({
+                roles,
+                user: u,
+                chat: createdChat,
+              })
+            );
+          }
+        } catch (e) {
+          return HttpResponse.json({ message: e?.message || "Server Error" }, { status: 500 });
+        }
+
+        return HttpResponse.json({ id: createdChat.id }, { status: 200 });
+      })
+    )
+  ),
+];
