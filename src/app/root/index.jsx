807bd548954c3507639c80765e8f3a5ef547bec8
crno <118383162+Crn0@users.noreply.github.com.>
2025-06-26 14:45:56 +0800

    refactor(app/root): check token existence and seed auth user cache in loader; show loading indicator in component

--- src/app/root/index.jsx
+++ src/app/root/index.jsx
@@ -1,5 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
 
+import { getAuthUserQueryOptions } from "../../lib";
+import { Spinner } from "../../components/ui/spinner";
+
 export default function AppRoot() {
+  const { isLoading } = useQuery({ ...getAuthUserQueryOptions() });
+
+  if (isLoading) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
+
   return <Outlet />;
 }
