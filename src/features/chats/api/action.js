a6089f85c348823b7959d5554b516b48b7afa780
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-02 14:25:02 +0800

    feat(chats/api): add createChat handler, validate getChats with Zod, and curry clientAction with tryCatch

--- src/features/chats/api/action.js
+++ src/features/chats/api/action.js
@@ -1,11 +1,14 @@
+import { tryCatch } from "../../../lib";
 import { createChat } from "./create-chat";
 
-export default async function clientAction({ request }) {
-  const formData = await request.clone().formData();
+export default function clientAction(queryClient) {
+  return async ({ request }) => {
+    const formData = await request.clone().formData();
 
-  const intent = formData.get("intent");
+    const intent = formData.get("intent");
 
-  if (intent.includes("create:chat")) return createChat(request);
+    if (intent.includes("create:chat")) return tryCatch(createChat(queryClient)(request));
 
-  throw new Error("Invalid intent");
+    throw new Error("Invalid intent");
+  };
 }
--- src/features/chats/api/index.js
+++ src/features/chats/api/index.js
@@ -1,3 +1,4 @@
 export { default as clientAction } from "./action";
 export * from "./create-chat";
 export * from "./get-chats";
+export * from "./get-chat";
