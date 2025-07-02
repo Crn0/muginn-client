8b93ef228ed446ccc5a71b31a3f7f31836b9caac
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-02 14:31:35 +0800

    refactor(layouts): delete ChatLayout and use ContentLayout

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,5 +1,4 @@
 export { default as AuthLayout } from "./auth-layout";
 export { default as SettingLayout } from "./setting-layout";
 export { default as DashBoardLayout } from "./dashboard-layout";
-export { default as ChatLayout } from "./chat-layout";
 export { default as ContentLayout } from "./content-layout";
