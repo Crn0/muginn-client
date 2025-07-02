9940ab66fe20c246cc923492559ef6ab1bbdba2b
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-02 17:01:44 +0800

    refactor(features/auth/api/loader): return null if status code is 401

--- src/features/auth/api/loader.js
+++ src/features/auth/api/loader.js
@@ -1,18 +1,13 @@
-import { replace } from "react-router-dom";
-
-import { paths } from "../../../configs";
 import { tryCatch } from "../../../lib";
 import refreshToken from "../../../lib/refresh-token";
 import { setToken } from "../../../stores";
 
-export const getRefreshToken = async (request) => {
+export const getRefreshToken = async () => {
   const { error, data: token } = await tryCatch(refreshToken());
-
-  const redirectTo = new URL(request.url).pathname;
 
   if (error?.code >= 500 || error?.message?.toLowerCase() === "failed to fetch") throw error;
 
-  if (error?.code === 401) return replace(paths.login.getHref(redirectTo));
+  if (error?.code === 401) return null;
 
   setToken(token);
 
