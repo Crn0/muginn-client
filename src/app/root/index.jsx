2d729673e060cdca2a3f48bee76ef225cb9f4bd7
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-02 17:00:32 +0800

    refactor(app/root): use Spinner component and ErrorElement; navigate to /chats/me when there's auth user

--- src/app/root/index.jsx
+++ src/app/root/index.jsx
@@ -1,11 +1,23 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { Suspense, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { Outlet } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
+import { paths } from "../../configs";
 import { getAuthUserQueryOptions } from "../../lib";
+import { ErrorElement } from "../../components/errors";
 import { Spinner } from "../../components/ui/spinner";
 
 export default function AppRoot() {
-  const { isLoading } = useQuery({ ...getAuthUserQueryOptions() });
+  const navigate = useNavigate();
+
+  const { isLoading, data } = useQuery({ ...getAuthUserQueryOptions() });
+
+  useEffect(() => {
+    if (data) {
+      navigate(paths.dashboard.me.getHref());
+    }
+  }, [data, navigate]);
 
   if (isLoading) {
     return (
@@ -15,5 +27,17 @@ export default function AppRoot() {
     );
   }
 
-  return <Outlet />;
+  return (
+    <Suspense
+      fallback={
+        <div>
+          <Spinner />
+        </div>
+      }
+    >
+      <ErrorBoundary fallback={<ErrorElement />}>
+        <Outlet />
+      </ErrorBoundary>
+    </Suspense>
+  );
 }
