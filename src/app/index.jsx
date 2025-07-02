df35a014bb1c96e2a4e66a76214385cfe2302b13
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-02 16:57:33 +0800

    refactor(app): change Router based component to object

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,20 @@
-import AppProvider from "./provider";
-import AppRouter from "./router";
+import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+
+import queryClient from "./query-client";
 
 export default function App() {
   return (
-    <AppProvider>
-      <AppRouter />
-    </AppProvider>
+    <Suspense fallback={<p>loading....</p>}>
+      <ErrorBoundary fallback={<p>error</p>}>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <Outlet />
+        </QueryClientProvider>
+      </ErrorBoundary>
+    </Suspense>
   );
 }
