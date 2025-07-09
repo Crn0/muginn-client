0844b0aab652a62b679a73632b828bf7970d3606
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-09 14:52:27 +0800

    feat(messages/component): add CreateMessage component

---
+++ src/features/messages/components/create-message.jsx
@@ -0,0 +1,116 @@
+import PropTypes from "prop-types";
+import { useRef, useState } from "react";
+import { FaPlusSquare, FaRegTrashAlt } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io";
+
+import { generateId } from "../../../lib";
+import { createMessageSchema, ACCEPTED_ATTACHMENTS_TYPES } from "../schema";
+import { useCreateMessage } from "../api/create-message";
+import { File, Form, FormError, Input } from "../../../components/ui/form";
+import { DropDownMenu } from "../../../components/ui/dropdown";
+import { Attachment } from "../../../components/ui/preview";
+import { Button } from "../../../components/ui/button";
+
+export default function CreateMessage({ chatId }) {
+  const createMessage = useCreateMessage(chatId);
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const fileRef = useRef();
+
+  const onSubmit = (data) => {
+    createMessage.mutate(data);
+  };
+
+  return (
+    <Form id='create-message-form' schema={createMessageSchema} onSubmit={onSubmit} mode='onBlur'>
+      {({ setValue, formState: { errors } }) => (
+        <>
+          <div>
+            {selectedFiles?.length > 0 && (
+              <div>
+                <div>
+                  {selectedFiles.map(({ id, file }) => (
+                    <div key={id}>
+                      <Attachment attachment={file} />
+                      <Button
+                        type='button'
+                        size='sm'
+                        className='bg-contain bg-no-repeat p-15'
+                        onClick={() => {
+                          const files = Array.from(selectedFiles).filter((f) => f.id !== id);
+
+                          setSelectedFiles(files);
+
+                          setValue("attachments", files);
+                        }}
+                      >
+                        <FaRegTrashAlt color='red' />
+                      </Button>
+                    </div>
+                  ))}
+                </div>
+                {errors?.attachments && <FormError errorMessage={errors.attachments.message} />}
+              </div>
+            )}
+          </div>
+          <div>
+            <DropDownMenu
+              renderButtonTrigger={(options) => (
+                <div>
+                  <Button
+                    type='button'
+                    testId='drop-down-trigger'
+                    onClick={options.onClick}
+                    ref={options.triggerRef}
+                  >
+                    <FaPlusSquare />
+                  </Button>
+                </div>
+              )}
+            >
+              <File
+                name='attachments'
+                label='Upload a File'
+                testId='message-attachments'
+                accept={ACCEPTED_ATTACHMENTS_TYPES.join(",")}
+                onKeyDown={(e) => e.code === "Enter" && fileRef.current.click()}
+                onChange={(e) => {
+                  const files = Array.from(e.target.files).map((file) => ({
+                    file,
+                    id: generateId(),
+                  }));
+
+                  setValue(
+                    "attachments",
+                    selectedFiles.concat(files).map(({ file }) => file)
+                  );
+
+                  setSelectedFiles((prev) => prev.concat(files));
+                }}
+                serverError={createMessage?.error}
+                ref={fileRef}
+                multiple
+              />
+            </DropDownMenu>
+
+            <div>
+              <Input
+                type='text'
+                name='content'
+                className='border border-black'
+                serverError={createMessage.error}
+              />
+            </div>
+
+            <Button type='submit'>
+              <IoIosSend />
+            </Button>
+          </div>
+        </>
+      )}
+    </Form>
+  );
+}
+
+CreateMessage.propTypes = {
+  chatId: PropTypes.string.isRequired,
+};
---
+++ src/features/messages/schema/create-message.js
@@ -0,0 +1,34 @@
+import z from "zod";
+
+const MAX_ATTACHMENTS_LENGTH = 5;
+const MAX_CONTENT_LENGTH = 2000;
+
+export const MAX_FILE_SIZE = 10_000_000; // 10mb
+
+export const ACCEPTED_ATTACHMENTS_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+  "application/epub+zip",
+  "application/pdf",
+];
+
+export const createMessageSchema = z.object({
+  content: z.string().trim().max(MAX_CONTENT_LENGTH),
+  attachments: z
+    .array(z.instanceof(File))
+    .max(MAX_ATTACHMENTS_LENGTH, {
+      message: `You can only upload ${MAX_ATTACHMENTS_LENGTH} files at a time!`,
+    })
+    .refine(
+      (files) => Array.from(files).every((file) => ACCEPTED_ATTACHMENTS_TYPES.includes(file.type)),
+      "Only .jpg, .jpeg, .png, .webp, epub and pdf formats are supported."
+    )
+    .refine(
+      (files) => Array.from(files).every((file) => file?.size <= MAX_FILE_SIZE),
+      "Max image size is 10MB"
+    )
+
+    .optional(),
+});
