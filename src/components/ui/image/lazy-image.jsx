1a29a277b2f8b70c981db3fda5c1c6ba2216ade2
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-09 14:17:24 +0800

    refactor(ui/image/lazy-image): nullish coalesce the mainImage

--- src/components/ui/image/lazy-image.jsx
+++ src/components/ui/image/lazy-image.jsx
@@ -17,7 +17,7 @@ export default function LazyImage({ asset, fallBackAsset, className, variant, si
 
   const sortedImages = [...(asset?.images || [])].sort((a, b) => a.size - b.size);
   const blurImage = sortedImages[0]?.url ?? fallBackAsset.lazyImage;
-  const mainImage = sortedImages[sortedImages.length - 1]?.url;
+  const mainImage = sortedImages[sortedImages.length - 1]?.url ?? asset?.url;
 
   const imageContent =
     mainImage === blurImage
