29d693d7374ecaae96f5124e8276fc0ff714bd44
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-09 15:00:13 +0800

    feat(features/messages/components): add Message component

---
+++ src/features/messages/components/__tests__/message.test.jsx
@@ -0,0 +1,27 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import { createMessages } from "./mocks/data";
+import Message from "../message";
+
+describe("Message Component", () => {
+  it("should render the messages user avatar, created date, content and attachments", () => {
+    const message = createMessages({ length: 1 });
+
+    render(<Message message={message} />);
+
+    expect(
+      screen.queryByRole("img", {
+        name: `${message.user.profile.displayName || message.user.username}'s nameplate avatar`,
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.queryByText(message.content)).toBeInTheDocument();
+
+    message.attachments.forEach((attachment) =>
+      expect(screen.queryByRole("img", { name: attachment.id })).toBeInTheDocument()
+    );
+
+    expect(screen.queryByRole("time")).toBeInTheDocument();
+  });
+});
