de9975fdea24b2630d98ce1a5b53bbbc4cc57e0d
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-09 14:05:41 +0800

    test(mocks): mock intersection observer

--- test/setup/setup.js
+++ test/setup/setup.js
@@ -2,6 +2,7 @@ import { expect, afterEach, beforeAll, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { seedUser, seedPermissions } from "../utils";
+import setupIntersectionObserverMock from "./intersection-observer";
 
 const { server } = await import("../mocks");
 
@@ -12,6 +13,7 @@ vi.mock("zustand");
 beforeAll(() => {
   server.listen({ onUnhandledRequest: "error" });
 
+  setupIntersectionObserverMock();
   seedUser();
   seedPermissions();
 });
