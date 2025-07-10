c48051151efd016e47a7bc5178238d202b1cbbf8
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-10 16:17:17 +0800

    refactor(hooks/useInfiniteScroll): use useState instead of useRef to track node for StrictMode compatibility

--- src/hooks/use-infinite-scroll.js
+++ src/hooks/use-infinite-scroll.js
@@ -1,42 +1,28 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function useInfiniteScroll(
   loadMore,
   isLoading,
   canLoadMore,
-  options = { root: null, rootMargin: "0px", delay: 0, threshold: 0 }
+  options = { root: null, rootMargin: "0px", threshold: 0, delay: 0 }
 ) {
-  const observer = useRef();
-  const prevNode = useRef();
+  const observer = useRef(null);
+  const [nodeRef, setNode] = useState(null);
 
-  const { root, rootMargin, threshold, delay } = options;
-
-  const setElementRef = useCallback(
-    (node) => {
-      if (isLoading || !canLoadMore) return;
-
-      if (prevNode.current) {
-        prevNode.current();
-        prevNode.current = undefined;
-      }
-
-      if (node && observer.current) {
-        observer.current.observe(node);
-
-        prevNode.current = () => observer.current.unobserve(node);
-      }
-    },
-    [isLoading, canLoadMore]
-  );
+  const setNodeRef = useCallback((node) => {
+    setNode(node);
+  }, []);
 
   useEffect(() => {
-    if (observer.current) observer.current.disconnect();
+    if (isLoading || !canLoadMore || !nodeRef) return () => {};
+
+    const { root, rootMargin, threshold, delay } = options;
 
     observer.current = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
           if (delay) {
-            setTimeout(() => loadMore(), delay);
+            setTimeout(loadMore, delay);
           } else {
             loadMore();
           }
@@ -45,47 +31,14 @@ export default function useInfiniteScroll(
       { root, rootMargin, threshold }
     );
 
-    return () => observer.current?.disconnect();
-  }, [loadMore, root, rootMargin, threshold, delay]);
+    observer.current.observe(nodeRef);
 
-  return setElementRef;
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [isLoading, canLoadMore, loadMore, options, nodeRef]);
+
+  return setNodeRef;
 }
-
-// export default function useInfiniteScroll(
-//   loadMore,
-//   isLoading,
-//   canLoadMore,
-//   options = { root: null, rootMargin: "0px", delay: 0, threshold: 0 }
-// ) {
-//   const observer = useRef();
-
-//   const setElementRef = useCallback(
-//     (node) => {
-//       if (isLoading || !canLoadMore) return;
-
-//       if (observer.current) observer.current.disconnect();
-
-//       if (!observer.current) {
-//         const { root, rootMargin, delay, threshold } = options;
-
-//         observer.current = new IntersectionObserver(
-//           ([entry]) => {
-//             if (entry.isIntersecting) {
-//               if (delay) {
-//                 setTimeout(loadMore, delay);
-//               } else {
-//                 loadMore();
-//               }
-//             }
-//           },
-//           { root, rootMargin, delay, threshold }
-//         );
-//       }
-
-//       if (node) observer.current.observe(node);
-//     },
-//     [isLoading, canLoadMore, options, loadMore]
-//   );
-
-//   return setElementRef;
-// }
