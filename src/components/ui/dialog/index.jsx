be158ed77427cf10699a00a37bf454adf27550eb
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-12 10:41:08 +0800

    feat(components/ui/dialog): add ModalDialog component

--- src/components/ui/dialog/index.jsx
+++ src/components/ui/dialog/index.jsx
@@ -1,2 +1,3 @@
 export { default as Dialog } from "./dialog";
 export { default as ConfirmationDialog } from "./confirmation-dialog";
+export { default as ModalDialog } from "./modal-dialog";
---
+++ src/components/ui/dialog/modal-dialog.jsx
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import { useRef } from "react";
+import { IoCloseCircleOutline } from "react-icons/io5";
+
+import Dialog from "./dialog";
+import { Button } from "../button";
+import { useDisclosureWithClickOutside } from "../../../hooks";
+
+export default function ModalDialog({ renderButtonTrigger, title, descriptions, children }) {
+  const dialogRef = useRef();
+  const triggerRef = useRef();
+
+  const { isOpen, toggle, close } = useDisclosureWithClickOutside(false, dialogRef, triggerRef);
+
+  const buttonTrigger = renderButtonTrigger({
+    triggerRef,
+    onClick: () => {
+      toggle();
+      triggerRef.current?.focus?.();
+    },
+  });
+
+  return (
+    <Dialog buttonTrigger={buttonTrigger} ref={dialogRef} open={isOpen}>
+      <>
+        <div>
+          <h2>{title}</h2>
+          <Button type='button' onClick={close}>
+            <IoCloseCircleOutline />
+          </Button>
+        </div>
+
+        {descriptions?.length > 0 && (
+          <div>
+            {descriptions.map((description) => (
+              <div key={description}>
+                <p>{description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <div>{children}</div>
+      </>
+    </Dialog>
+  );
+}
+
+ModalDialog.propTypes = {
+  renderButtonTrigger: PropTypes.func,
+  title: PropTypes.string.isRequired,
+  descriptions: PropTypes.arrayOf([PropTypes.string]),
+  children: PropTypes.element.isRequired,
+};
