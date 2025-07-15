7ce51c124eace0bf9e3d97d64bd270332d2ebf8a
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-15 16:59:58 +0800

    feat(ui/link): add onClick prop on Link component

--- src/components/ui/link/link.jsx
+++ src/components/ui/link/link.jsx
@@ -3,11 +3,12 @@ import { Link as RouterLink } from "react-router-dom";
 
 import { cn } from "../../../utils";
 
-export default function Link({ to, children, className }) {
+export default function Link({ to, children, className, onClick = () => {} }) {
   return (
     <RouterLink
       hrefLang=''
       to={to}
+      onClick={onClick}
       className={cn(
         "inline-flex items-center justify-center text-sm font-medium whitespace-nowrap text-blue-500 transition-colors hover:opacity-75 focus-visible:ring-4 focus-visible:ring-blue-500 focus-visible:outline-none",
         className
@@ -22,4 +23,5 @@ Link.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  onClick: PropTypes.func,
 };
