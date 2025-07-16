02937a9ef7b12af23cac3e24685b7eeeaf4f092a
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-16 14:16:53 +0800

    feat(hooks): add useResponsiveDrawer for handling responsive drawer state

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,4 +3,5 @@ export { default as useDisclosureWithClickOutside } from "./use-disclosure-click
 export { default as useRefreshToken } from "./use-refresh-token";
 export { default as useInfiniteScroll } from "./use-infinite-scroll";
 export { default as useClickOutside } from "./use-click-outside";
+export { default as useResponsiveDrawer } from "./use-responsive-drawer";
 export * from "./use-input-error-handler";
---
+++ src/hooks/use-responsive-drawer.js
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+export default function useResponsiveDrawer(screenSize) {
+  const [isAutoDrawer, setIsAutoDrawer] = useState(true);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [clientWidth, setClientWidth] = useState(window.innerWidth);
+
+  const close = useCallback(() => setIsDrawerOpen(false), []);
+  const open = useCallback(() => setIsDrawerOpen(true), []);
+  const toggle = useCallback(() => setIsDrawerOpen((state) => !state), []);
+  const auto = useCallback(() => setIsAutoDrawer(true), []);
+  const manual = useCallback(() => setIsAutoDrawer(false), []);
+
+  const state = useMemo(
+    () => ({
+      isAutoDrawer,
+      isDrawerOpen,
+      clientWidth,
+      close,
+      open,
+      toggle,
+      auto,
+      manual,
+    }),
+    [auto, clientWidth, close, isAutoDrawer, isDrawerOpen, manual, open, toggle]
+  );
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    window.addEventListener(
+      "resize",
+      () => setClientWidth(window.innerWidth),
+
+      { signal }
+    );
+
+    return () => controller.abort();
+  }, []);
+
+  useEffect(() => {
+    if (clientWidth < screenSize && isAutoDrawer && !isDrawerOpen) {
+      setIsDrawerOpen(true);
+    } else if (clientWidth >= screenSize && !isDrawerOpen) {
+      setIsDrawerOpen(false);
+    }
+  }, [clientWidth, isAutoDrawer, isDrawerOpen, screenSize]);
+
+  return state;
+}
