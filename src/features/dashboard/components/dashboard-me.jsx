ed1839ced1be4b9445aa5c77e96309f8e05e3967
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-16 16:17:40 +0800

    refactor(layouts/dashboard-me): use Outlet with ErrorBoundary

--- src/features/dashboard/components/dashboard-me.jsx
+++ src/features/dashboard/components/dashboard-me.jsx
@@ -1,20 +1,12 @@
 import { ErrorBoundary } from "react-error-boundary";
+import { Outlet } from "react-router-dom";
 
 import { ErrorElement } from "../../../components/errors";
-import { ContentLayout } from "../../../components/layouts";
-import DirectChatList from "../../chats/components/direct-chat-list";
 
 export default function DashBoardMe() {
   return (
-    <ContentLayout
-      header={
-        <>
-          <h2>Direct Messages</h2>
-          <ErrorBoundary fallbackRender={ErrorElement}>
-            <DirectChatList />
-          </ErrorBoundary>
-        </>
-      }
-    />
+    <ErrorBoundary fallbackRender={ErrorElement}>
+      <Outlet />
+    </ErrorBoundary>
   );
 }
