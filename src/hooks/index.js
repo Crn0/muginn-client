2295755151ac709cdf2169cf8564bb7f99f448d5
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-16 14:14:36 +0800

    chore(hooks/index): export useClickOutside hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,4 +2,5 @@ export { default as useSilentLogin } from "./user-silent-login";
 export { default as useDisclosureWithClickOutside } from "./use-disclosure-click-outside";
 export { default as useRefreshToken } from "./use-refresh-token";
 export { default as useInfiniteScroll } from "./use-infinite-scroll";
+export { default as useClickOutside } from "./use-click-outside";
 export * from "./use-input-error-handler";
