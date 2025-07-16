6ec6b615786c90be575484451a65c289da54061c
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-16 14:24:24 +0800

    feat(components/ui/drawer): add Drawer component

---
+++ src/components/ui/drawer/drawer.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import { useRef } from "react";
+
+import { cn } from "../../../utils";
+import { useClickOutside } from "../../../hooks";
+
+export default function Drawer({ triggerRef, className, open, onClose, children }) {
+  const drawerRef = useRef();
+
+  useClickOutside(drawerRef, triggerRef, onClose);
+
+  return (
+    <div
+      tabIndex='-1'
+      ref={drawerRef}
+      className={cn(
+        "translate z-50 max-w-full -translate-x-full bg-black opacity-0 shadow-lg transition delay-75 duration-75 ease-in-out sm:hidden",
+        `${open ? "flex-1 translate-0 transform opacity-100" : "hidden"}`,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+Drawer.propTypes = {
+  triggerRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]),
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)])
+    .isRequired,
+};
