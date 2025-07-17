832508ac3e815f66e12062546a96dd911e02c15b
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-17 18:06:10 +0800

    style(ui/form-error): center text

--- src/components/ui/form/error.jsx
+++ src/components/ui/form/error.jsx
@@ -4,7 +4,11 @@ export default function Error({ errorMessage }) {
   if (!errorMessage) return null;
 
   return (
-    <div role='alert' aria-label={errorMessage} className='text-sm font-semibold text-red-500'>
+    <div
+      role='alert'
+      aria-label={errorMessage}
+      className='text-center text-sm font-semibold text-red-500'
+    >
       {errorMessage}
     </div>
   );
