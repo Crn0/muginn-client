b82de696fbbe784041c19696fbc0e54870554a69
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-19 13:23:36 +0800

    refactor(messages/schema/create-message): handle empty file input and set default value for content

--- src/features/messages/schema/create-message.js
+++ src/features/messages/schema/create-message.js
@@ -15,20 +15,29 @@ export const ACCEPTED_ATTACHMENTS_TYPES = [
 ];
 
 export const createMessageSchema = z.object({
-  content: z.string().trim().max(MAX_CONTENT_LENGTH),
-  attachments: z
-    .array(z.instanceof(File))
-    .max(MAX_ATTACHMENTS_LENGTH, {
-      message: `You can only upload ${MAX_ATTACHMENTS_LENGTH} files at a time!`,
-    })
-    .refine(
-      (files) => Array.from(files).every((file) => ACCEPTED_ATTACHMENTS_TYPES.includes(file.type)),
-      "Only .jpg, .jpeg, .png, .webp, epub and pdf formats are supported."
-    )
-    .refine(
-      (files) => Array.from(files).every((file) => file?.size <= MAX_FILE_SIZE),
-      "Max image size is 10MB"
-    )
+  content: z.string().trim().max(MAX_CONTENT_LENGTH).default(""),
+  attachments: z.preprocess(
+    (value) => {
+      if (!value) return [];
 
-    .optional(),
+      if (value instanceof FileList && value.length <= 0) return [];
+
+      return Array.isArray(value) ? value : [value];
+    },
+    z
+      .array(z.instanceof(File))
+      .max(MAX_ATTACHMENTS_LENGTH, {
+        message: `You can only upload ${MAX_ATTACHMENTS_LENGTH} files at a time!`,
+      })
+      .refine(
+        (files) =>
+          Array.from(files).every((file) => ACCEPTED_ATTACHMENTS_TYPES.includes(file.type)),
+        "Only .jpg, .jpeg, .png, .webp, epub and pdf formats are supported."
+      )
+      .refine(
+        (files) => Array.from(files).every((file) => file?.size <= MAX_FILE_SIZE),
+        "Max image size is 10MB"
+      )
+      .optional()
+  ),
 });
