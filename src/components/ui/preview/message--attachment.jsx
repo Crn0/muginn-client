6b51c2a3c53099ac4962df66699b1cd3c68155c2
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-20 10:15:13 +0800

    fix(ui/preview/message-attachment): change the component to just handle message attachment

--- src/components/ui/preview/message--attachment.jsx
+++ src/components/ui/preview/message--attachment.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { useEffect, useMemo, useState } from "react";
 
 import { cn } from "../../../utils";
+import getAttachment from "./get-attachment";
 import { LazyImage } from "../image";
 
 import defaultImage from "../../../assets/default-image.png";
@@ -10,45 +11,25 @@ import defaultImageLazy from "../../../assets/default-image-lazy.png";
 import epubSvg from "../../../assets/epub.svg";
 import pdfSvg from "../../../assets/pdf.svg";
 
-const fallbackPreview = {
+const fallback = {
   Pdf: { image: pdfSvg, lazyImage: pdfSvg },
   Epub: { image: epubSvg, lazyImage: epubSvg },
   Image: { image: defaultImage, lazyImage: defaultImageLazy },
 };
 
-const getFallbackAsset = (type) =>
-  fallbackPreview[type] ?? { image: defaultImage, lazyImage: defaultImageLazy };
-
 export default function MessageAttachment({ attachment, className }) {
-  const [objectUrl, setObjectUrl] = useState(null);
-
-  useEffect(() => {
-    if (attachment instanceof File) {
-      const url = URL.createObjectURL(attachment);
-
-      setObjectUrl(url);
-
-      return () => {
-        URL.revokeObjectURL(url);
-      };
-    }
-
-    return () => {};
-  }, [attachment]);
-
-  const asset = useMemo(
-    () =>
-      attachment instanceof File
-        ? { url: objectUrl ?? fallbackPreview[attachment.type] }
-        : attachment,
-    [attachment, objectUrl]
+  const { mainImage, lazyImage } = getAttachment(
+    attachment,
+    fallback[attachment.type] ?? fallback.Image
   );
 
   return (
     <div className={cn(className)}>
       <LazyImage
-        asset={asset}
-        fallBackAsset={getFallbackAsset(attachment?.type)}
+        className='rounded-md'
+        mainImage={mainImage}
+        lazyImage={lazyImage}
+        variant='attachment'
         alt={attachment.id}
       />
     </div>
