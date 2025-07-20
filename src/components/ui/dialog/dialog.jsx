09eccc971c727c88e402d272c882d062064c7435
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-20 13:09:19 +0800

    style(ui/dialog): add position fixed on larger screen

--- src/components/ui/dialog/dialog.jsx
+++ src/components/ui/dialog/dialog.jsx
@@ -13,7 +13,7 @@ const Dialog = forwardRef(({ parentId, id, className, open, buttonTrigger, child
           id={id}
           data-inside-portal='true'
           className={cn(
-            "focus-visible:ring-ring z-50 min-h-100 w-[100%] flex-2 gap-5 place-self-center-safe self-center-safe justify-self-center-safe border border-gray-900 bg-black p-5 text-white focus-visible:ring-4 focus-visible:ring-blue-500 sm:w-lg",
+            "focus-visible:ring-ring z-50 min-h-100 w-[100%] flex-2 gap-5 place-self-center-safe self-center-safe justify-self-center-safe border border-gray-900 bg-black p-5 text-white focus-visible:ring-4 focus-visible:ring-blue-500 sm:fixed sm:top-25 sm:w-lg",
             `${open ? "grid" : "none"}`,
             className
           )}
