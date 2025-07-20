e04edb3c9364ff3c470c89ed42bea908559c16ca
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-20 10:34:41 +0800

    fix(messages/components/__tests__/create-message.test): remove stale assertion

--- src/features/messages/components/__tests__/create-message.test.jsx
+++ src/features/messages/components/__tests__/create-message.test.jsx
@@ -12,12 +12,13 @@ const queryClient = new QueryClient({ defaultOptions: { queries: { retry: false 
 const chat = createChats({ ownerId: faker.string.uuid(), length: 1 })[0];
 
 describe("Message Creation", () => {
-  it("should render the Form component with input text and drop menu button", () => {
+  it("should render the Form component", () => {
     renderComponent(<CreateMessage chatId={chat.id} />, queryClient);
 
-    const form = screen.getByRole("form");
+    const form = screen.queryByRole("form");
+
+    expect(form).toBeInTheDocument();
 
     expect(within(form).getByRole("textbox")).toBeInTheDocument();
-    expect(within(form).getByTestId("drop-down-trigger")).toBeInTheDocument();
   });
 });
