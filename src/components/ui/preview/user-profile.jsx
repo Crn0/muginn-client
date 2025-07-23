82ad3050e390fcf6cc69976c258d059abda16bac
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-23 10:56:28 +0800

    feat(ui/preview/user-profile): add styles and className prop; remove unnecessary div wrappers

--- src/components/ui/preview/user-profile.jsx
+++ src/components/ui/preview/user-profile.jsx
@@ -2,49 +2,44 @@ import PropTypes from "prop-types";
 
 import { UserAvatar, BackgroundAvatar } from "../image";
 
-export default function UserProfilePreview({ user, renderProfileButton, children }) {
+export default function UserProfilePreview({ user, renderProfileButton, className, children }) {
   const profileButton = renderProfileButton();
 
   return (
-    <div data-testid='user-profile-preview'>
-      <div>
-        <header>
-          <div>
-            <BackgroundAvatar
-              asset={user.profile.backgroundAvatar}
-              alt='Profile background'
-              type='user'
-            />
-          </div>
-          <div>
-            <UserAvatar
-              asset={user.profile.avatar}
-              alt={`${user.profile.displayName || user.username}'s avatar`}
-              type='user'
-            />
-          </div>
-        </header>
+    <div className={className}>
+      <div
+        className='grid gap-5 rounded-full border border-slate-900 p-2'
+        data-testid='user-profile-preview'
+      >
+        <BackgroundAvatar
+          asset={user.profile.backgroundAvatar}
+          alt='Profile background'
+          type='user'
+        />
 
-        <main>
-          <div>
-            <h3>{user.profile.displayName}</h3>
+        <UserAvatar
+          asset={user.profile.avatar}
+          alt={`${user.profile.displayName || user.username}'s avatar`}
+          type='user'
+          className='-mt-5 w-20 rounded-full bg-black'
+        />
 
-            <div>
-              <span>{user.username}</span>
-            </div>
+        <div>
+          <div>
+            <h3 className='font-bold'>{user.profile.displayName}</h3>
+
+            <p className='font-extralight'>{user.username}</p>
           </div>
 
           <div>
             <p>{user.profile.aboutMe}</p>
           </div>
-        </main>
+        </div>
 
-        <footer>
-          <div>{profileButton}</div>
-        </footer>
+        {profileButton}
       </div>
 
-      {children && <div>{children}</div>}
+      {children}
     </div>
   );
 }
@@ -78,5 +73,6 @@ UserProfilePreview.propTypes = {
     }).isRequired,
   }).isRequired,
   renderProfileButton: PropTypes.func.isRequired,
-  children: PropTypes.element,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
 };
