1f5ee2db7cebeea026d585d3dcdd4130c1f2bb55
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-27 13:10:52 +0800

    feat(chats/messages): add Authorization checks to LeaveGroupChat and DeleteMessage components

--- src/features/messages/components/delete-message.jsx
+++ src/features/messages/components/delete-message.jsx
@@ -1,71 +1,96 @@
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
 
 import { formatDate } from "../../../utils";
+import { useChat, useMyMembership } from "../../chats/api";
+import { Authorization, permissions, policies } from "../../../lib";
 import { useDeleteMessage } from "../api/delete-message";
 import { useDropDownMenu } from "../../../components/ui/dropdown/context/dropdown-menu-context";
 import { ConfirmationDialog } from "../../../components/ui/dialog";
 import { Button } from "../../../components/ui/button";
 import { NameplatePreview } from "../../../components/ui/preview";
+import { Spinner } from "../../../components/ui/spinner";
 import MessageAttachments from "./message-attachments";
 
 export default function DeleteMessage({ message }) {
+  const { chatId } = useParams();
+
+  const { data: chat } = useChat(chatId);
+
+  const membershipQuery = useMyMembership(chat.id);
   const deleteMessage = useDeleteMessage({ chatId: message.chatId, messageId: message.id });
+
   const { hide, reset } = useDropDownMenu();
 
+  if (membershipQuery.isLoading && !membershipQuery.data) {
+    return (
+      <div className='row-span-3 flex items-center-safe justify-center-safe'>
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
-    <ConfirmationDialog
-      parentId={message.id}
-      isDone={deleteMessage.isSuccess}
-      className='fixed top-5'
-      title='Delete Message'
-      icon='danger'
-      body={
-        <div className='grid gap-2 border-2 border-slate-900 p-2'>
-          <div className='flex items-center-safe justify-between'>
-            <div className='flex items-center-safe gap-2'>
-              <NameplatePreview
-                username={message.user.username}
-                displayName={message.user.profile.displayName}
-                asset={message.user.profile.avatar}
-              />
-              <time className='text-xs font-light' dateTime={message.createdAt}>
-                {formatDate(message.createdAt)}
-              </time>
+    <Authorization
+      policies={policies}
+      user={membershipQuery.data}
+      resource='message'
+      action='delete'
+      data={message}
+      environment={{ chat, permissions: permissions.message.delete }}
+    >
+      <ConfirmationDialog
+        parentId={message.id}
+        isDone={deleteMessage.isSuccess}
+        className='fixed top-50'
+        title='Delete Message'
+        icon='danger'
+        body={
+          <div className='grid gap-2 border-2 border-slate-900 p-2'>
+            <div className='flex items-center-safe justify-between'>
+              <div className='flex items-center-safe gap-2'>
+                <NameplatePreview
+                  username={message.user.username}
+                  displayName={message.user.profile.displayName}
+                  asset={message.user.profile.avatar}
+                />
+                <time className='text-xs font-light' dateTime={message.createdAt}>
+                  {formatDate(message.createdAt)}
+                </time>
+              </div>
+            </div>
+
+            <div>
+              <p>{message.content}</p>
+
+              <MessageAttachments attachments={message.attachments} className='w-50 sm:w-sm' />
             </div>
           </div>
-
-          <div>
-            <p className='w-50 overflow-hidden overflow-ellipsis whitespace-nowrap'>
-              {message.content}
-            </p>
-
-            <MessageAttachments attachments={message.attachments} className='w-50 sm:w-sm' />
-          </div>
-        </div>
-      }
-      onCancel={reset}
-      renderButtonTrigger={({ onClick }) => (
-        <Button
-          type='button'
-          variant='outline-destructive'
-          onClick={() => {
-            onClick();
-            hide();
-          }}
-        >
-          Delete Message
-        </Button>
-      )}
-      confirmButton={
-        <Button
-          type='button'
-          variant='destructive'
-          onClick={() => deleteMessage.mutate({ chatId: message.chatId, messageId: message.id })}
-        >
-          Delete
-        </Button>
-      }
-    />
+        }
+        onCancel={reset}
+        renderButtonTrigger={({ onClick }) => (
+          <Button
+            type='button'
+            variant='outline-destructive'
+            onClick={() => {
+              onClick();
+              hide();
+            }}
+          >
+            Delete Message
+          </Button>
+        )}
+        confirmButton={
+          <Button
+            type='button'
+            variant='destructive'
+            onClick={() => deleteMessage.mutate({ chatId: message.chatId, messageId: message.id })}
+          >
+            Delete
+          </Button>
+        }
+      />
+    </Authorization>
   );
 }
 
