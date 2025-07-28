9110e2565d698a31f1b83be9960eb36ddd6d449d
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-28 15:04:16 +0800

    refactor(chats/components/group-chat-header): replace GroupChatAvatar with background image styling

--- src/features/chats/components/group-chat-header.jsx
+++ src/features/chats/components/group-chat-header.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { IoCloseSharp } from "react-icons/io5";
 
+import { cn } from "../../../utils";
 import { useChat } from "../api";
 import { Spinner } from "../../../components/ui/spinner";
-import { GroupChatAvatar } from "../../../components/ui/image";
 import { DropDownMenu } from "../../../components/ui/dropdown";
 import { Button } from "../../../components/ui/button";
 
@@ -27,35 +27,37 @@ export default function GroupChatHeader({ chatId }) {
 
   return (
     <div className='flex flex-1 p-2'>
-      <GroupChatAvatar
-        asset={chat.avatar}
-        alt={chat.name}
-        type='group'
-        variant='banner'
-        size='lg'
-        className='w-[90%]'
-      />
       <div className='flex flex-1 flex-col border border-b-0 border-slate-900 p-2 sm:p-5'>
         <DropDownMenu
           id='group-chat-view-dropdown'
-          className='p-1'
+          className='fixed top-15 bg-black p-1 sm:top-30'
           renderButtonTrigger={(options) => (
-            <Button
-              type='button'
-              testId='chat-drop-down-trigger'
-              onClick={options.onClick}
-              ref={options.triggerRef}
-              variant='outline'
-              size='lg'
-              className='flex w-full justify-between gap-2 border-2 border-gray-900 p-5 text-lg text-white hover:bg-gray-200/20 sm:w-lg'
+            <div
+              className='flex-1 rounded-md bg-contain bg-no-repeat'
+              style={{
+                ...(chat.avatar ? { backgroundImage: `url(${chat.avatar.url})` } : {}),
+              }}
             >
-              <>
-                <p className='overflow-hidden'>{chat.name}</p>
-                <p className='text-lg'>
-                  {!options.isOpen ? <MdOutlineKeyboardArrowDown /> : <IoCloseSharp />}
-                </p>
-              </>
-            </Button>
+              <Button
+                type='button'
+                testId='chat-drop-down-trigger'
+                onClick={options.onClick}
+                ref={options.triggerRef}
+                variant='outline'
+                size='lg'
+                className={cn(
+                  "flex w-full justify-between gap-2 border-2 border-gray-900 p-5 text-lg text-white hover:bg-gray-200/20 sm:w-lg",
+                  `${!chat.avatar ? "" : "border-2 border-none bg-black/20 hover:bg-black/50 hover:backdrop-blur-md sm:border-0"}`
+                )}
+              >
+                <>
+                  <p className='overflow-hidden'>{chat.name}</p>
+                  <p className='text-lg'>
+                    {!options.isOpen ? <MdOutlineKeyboardArrowDown /> : <IoCloseSharp />}
+                  </p>
+                </>
+              </Button>
+            </div>
           )}
         >
           <LeaveGroupChat chat={chat} />
