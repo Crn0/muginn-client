a635e8f1d90d8a654592aa65989f35d63c003dbe
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-28 19:20:00 +0800

    fix(lib/policies/chat-policy): remove chat argument from permissionCheck

--- src/lib/policies/chat-policy.js
+++ src/lib/policies/chat-policy.js
@@ -4,7 +4,7 @@ export default {
   update: (user, chat, { permissions }) => {
     if (user.id === chat.ownerId) return true;
 
-    return permissionCheck(user, chat, permissions);
+    return permissionCheck(user, permissions);
   },
   leave: (user, chat) => user.id !== chat.ownerId,
   delete: (user, chat) => user.id === chat?.ownerId,
