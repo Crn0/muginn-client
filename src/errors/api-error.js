8e0922eedc0199fa3505c543c01570ef0df37701
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-28 19:20:57 +0800

    refactor(errors/api-error): rename to Api Error

--- src/errors/api-error.js
+++ src/errors/api-error.js
@@ -1,7 +1,7 @@
 import CustomError from "./custom-error";
 
 export default class ApiError extends CustomError {
-  constructor({ message, response, data, code, name = "Validation Error", isOperational = true }) {
+  constructor({ message, response, data, code, name = "Api Error", isOperational = true }) {
     super(name, isOperational, message, code, response, data);
   }
 }
