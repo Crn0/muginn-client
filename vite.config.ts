0192cbc6bf75d8e57437a6634fa79fe871d7fcab
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-13 17:59:03 +0800

    refactor(vite.config): add type reference and path alias; convert vite.config and setupFiles to Typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "/test/setup/setup.js",
+    setupFiles: "./test/setup/setup.ts",
+    exclude: ["*.test.jsx", "node_modules"],
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
