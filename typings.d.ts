4fb94cad37dc9ebac9746053485028384e5662f2
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-13 17:54:03 +0800

    feat(types): add typings.d.ts

---
+++ typings.d.ts
@@ -0,0 +1,8 @@
+declare module "*.css";
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.gif";
+declare module "*.jpeg";
+declare module "*.epub";
+declare module "*.pdf";
+declare module "*.svg";
