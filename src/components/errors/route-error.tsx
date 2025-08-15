79fa176de97b27906d0f2bb7277e1837ff30b889
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-15 16:45:12 +0800

    refactor(components/errors): migrate error components to Typescript

--- src/components/errors/route-error.tsx
+++ src/components/errors/route-error.tsx
@@ -1,13 +1,14 @@
+import type { CustomError } from "@/errors/custom-error";
 import { useRouteError } from "react-router-dom";
 
 export default function RouteErrorElement() {
-  const error = useRouteError();
+  const error = useRouteError() as CustomError;
 
   return (
     <div className='grid h-dvh place-content-center place-items-center bg-black text-white'>
       <div role='alert'>
         <p className='font-extralight italic'>Something went wrong:</p>
-        <pre className='text-red-600'>{error?.message || error?.statusText || "Unknown error"}</pre>
+        <pre className='text-red-600'>{error?.message || "Unknown error"}</pre>
       </div>
     </div>
   );
