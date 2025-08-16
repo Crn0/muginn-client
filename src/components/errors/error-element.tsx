bf2af71bb38e3b66d2bb969a1574d3427f7ecc60
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-16 18:59:20 +0800

    fix(errors/error-element): use type predicate to handle CustomError sub class and getErrorCode to handle error codes

--- src/components/errors/error-element.tsx
+++ src/components/errors/error-element.tsx
@@ -1,16 +1,29 @@
 import { useErrorBoundary } from "react-error-boundary";
 
-import { ValidationError, type CustomError } from "@/errors";
+import { CustomError, ValidationError } from "@/errors";
 import { Button } from "@/components/ui/button";
 
-export function ErrorElement({ error }: { error: InstanceType<typeof CustomError> }) {
+const isCustomError = (e: Error | CustomError): e is CustomError =>
+  (e as CustomError)?.is !== undefined;
+
+const getErrorCode = (e: Error | CustomError) => {
+  const defaultCode = 500;
+
+  if (isCustomError(e)) {
+    return e.code || e.response?.status || defaultCode;
+  }
+
+  return defaultCode;
+};
+
+export function ErrorElement({ error }: { error: InstanceType<typeof Error> }) {
   const { resetBoundary } = useErrorBoundary();
 
   return (
     <div role='alert'>
-      <p>Error {error.code || error?.response?.status || 422}: Something went wrong</p>
+      <p>Error {getErrorCode(error)}: Something went wrong</p>
       <pre className='text-red-600'>{error.message}</pre>
-      {error.is(ValidationError) && error.fields?.length > 0 && (
+      {isCustomError(error) && error.is(ValidationError) && error.fields?.length > 0 && (
         <ul className='ml-4 list-disc'>
           {error.fields.map((field) => (
             <li key={`${field.path?.join(".")}:${field.message}`}>{field.message}</li>
