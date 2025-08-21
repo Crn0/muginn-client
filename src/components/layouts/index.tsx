96f3949a8a2fa35e66854408496d27714427dcdf
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-21 17:15:32 +0800

    refactor(layouts/setting-layout): migrate SettingLayout component to Typescript

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,5 +1,5 @@
 export * from "./auth-layout";
 export * from "./chat-layout";
-export { default as SettingLayout } from "./setting-layout";
+export * from "./setting-layout";
 export * from "./dashboard-layout";
 export * from "./content-layout";
